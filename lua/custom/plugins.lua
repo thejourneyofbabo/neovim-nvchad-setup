770b244dca37285cc58ee8026f4f7b8c16c1b34a
Jisang Yun <journeyofbabo@gmail.com>
2024-01-16 22:09:36 +0900

    shortcut change

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -31,12 +31,12 @@ local plugins = {
         -- optionally use on_attach to set keymaps when aerial has attached to a buffer
         on_attach = function(bufnr)
           -- Jump forwards/backwards with '{' and '}'
-        vim.keymap.set("n", "<leader>ak", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "<leader>aj", "<cmd>AerialNext<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
         end,
       })
       -- You probably also want to set a keymap to toggle aerial
-      vim.keymap.set("n", "<leader>aa", "<cmd>AerialToggle!<CR>")
+      vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
     end,
     lazy = false
   },
