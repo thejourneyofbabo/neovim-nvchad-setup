8454e65a4e35d169dad46b37f7c09c23e0c288c9
Jisang Yun <journeyofbabo@gmail.com>
2024-01-23 11:12:18 +0900

    python setup

---
+++ custom/plugins/rust-setup.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+
+    config = function(_, opts)
+      local rt = require("rust-tools")
+      local mason_registry = require("mason-registry")
+
+      local codelldb = mason_registry.get_package("codelldb")
+      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+      
+      rt.setup({
+        dap = {
+          adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+        server = {
+          on_attach = function(_, bufnr)
+            vim.keymap.set('n', '<leader>k', rt.hover_actions.hover_actions, { buffer = bufnr },{})
+            vim.keymap.set('n', '<leader>q', rt.code_action_group.code_action_group, { buffer = bufnr }, {})
+          end,
+        },
+        tools = {
+          hover_actions = {
+            auto_focus = true,
+          }
+        }
+      })
+    end,
+    lazy = false
+  },
+  {
+    "saecki/crates.nvim",
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  },
+}
